b7fca8745ccd28bd9bc756c68df7b0059fa29c93
FedericoMizzau <federicomizzau.77@gmail.com>
2023-07-15 19:19:48 -0300

    Primer commit- estructura inicial

---
+++ src/components/Pacientes/Pacientes.jsx
@@ -0,0 +1,14 @@
+import {React, useState, useEffect} from 'react'
+
+const Pacientes = () => {
+  const [accion, setAccion] = useState("")
+
+    return (
+    <div>
+        
+    
+    </div>
+  )
+}
+
+export default Pacientes
---
+++ src/components/Pacientes/PacientesBuscar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PacientesBuscar = () => {
+  return (
+    <form>
+        
+    </form>
+  )
+}
+
+export default PacientesBuscar
